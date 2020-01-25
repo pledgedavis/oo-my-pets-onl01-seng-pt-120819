4e0deb42142ac5564edd5fbcc13880f364d14490
Pledge Davis <pledgedavis14@gmail.com>
2020-01-25 15:22:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
+require 'pry'
 class Owner
   # code goes here
+     attr_reader :name , :species 
+     @@all =[]
+     def initialize(name)
+       @name = name
+       @species = "human"
+     @@all << self
+     
+    end
+    
+    def say_species
+      
+      "I am a #{species}."
+      
+    end
+    
+    def self.all
+     @@all
+   end    
+   def self.count
+   
+   @@all.count
+  
+   end
+   
+   def self.reset_all 
+    # @@all.destroy
+     @@all.clear
+   end
+   
+  # def 
+   def cats
+  # binding.pry
+  # self.Cat
+    Cat.all.select {|cats| cats.owner == self}
+   end   
+   
+   def dogs
+   
+   Dog.all.select {|dogs| dogs.owner == self}
+   end
+   
+   def buy_cat(name)
+  # binding.pry
+  Cat.new(name, self)
+   end
+   
+   def buy_dog(name)
+     Dog.new(name, self)
+     
+   end
+   
+    def walk_dogs
+      # binding.pry
+      dogs.each {|dog| dog.mood ="happy"}
+      # dog
+    end
+   def feed_cats
+     cats.each {|cat| cat.mood ="happy"}
+   end
+   def sell_pets
+   binding.pry
+  # dogs.each {|dog| dog.mood ="nervous"}
+   end
 end
+
+
+
+
